07be10f54b3e0279f0dd8517fb04e7625fd38f1f
Bart Massey <bart.massey@gmail.com>
2026-01-07 16:31:48 -0800

    started tutorial over

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use prompted::input;
+
+fn greaten(name: String) -> String {
+    name + " The Great"
+}
+
+fn smallen(name: String) -> String {
+    name + " the small"
+}
+
 fn main() {
-    println!("Hello, world!");
+    let name = input!("Who are you?: ");
+    let name = if name.chars().next().unwrap().is_uppercase() {
+        greaten(name)
+    } else {
+        smallen(name)
+    };
+    let greetings = ["Helooo…", "Howdy", "Hi"];
+    for g in greetings {
+        println!("{}", g);
+    }
+    println!("Hello, {}!", name);
+    // loop {}
 }
