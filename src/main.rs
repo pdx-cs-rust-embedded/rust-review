d9cf08f2c7eadb0b186924fa681cddfc4be875a0
Bart Massey <bart.massey@gmail.com>
2026-01-07 17:22:49 -0800

    finished day 1 stuff

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,45 @@
 use prompted::input;
 
+/// Add "The Great" to the end of the name.
 fn greaten(name: String) -> String {
     name + " The Great"
 }
 
+/// Add "the small" to the end of the name.
 fn smallen(name: String) -> String {
     name + " the small"
 }
 
 fn main() {
-    let name = input!("Who are you?: ");
-    let name = if name.chars().next().unwrap().is_uppercase() {
-        greaten(name)
+    let base_name = input!("Who are you?: ");
+    let name = if base_name.chars().next().unwrap().is_uppercase() {
+        greaten(base_name.clone())
     } else {
-        smallen(name)
+        smallen(base_name.clone())
     };
     let greetings = ["Helooo…", "Howdy", "Hi"];
     for g in greetings {
         println!("{}", g);
     }
-    println!("Hello, {}!", name);
-    // loop {}
+    println!("Hello {}… {}!", base_name, name);
+
+    /*
+    u8, u16, u32, u64, u128,
+    i8, i16, i32, i64, i128,
+    usize, isize
+    */
+
+    /*
+    memory
+      global RAM
+      global ROM ("flash")
+      stack
+      heap
+    register
+    */
+
+    let x = 1_000_u128;
+    let y = 1_000;
+    let z = x + y;
+    println!("{} {} {}", x, y, z);
 }
