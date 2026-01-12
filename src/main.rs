c527a3875891fd3cfbdd277235f15634da36ae60
Bart Massey <bart.massey@gmail.com>
2026-01-12 15:54:30 -0800

    added struct demo

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
 use prompted::input;
 
+struct Point {
+    x: u32,
+    y: u32,
+}
+
+impl Point {
+    fn new(x: u32, y: u32) -> Point {
+        Point { x, y }
+    }
+
+    fn double(&self) -> Point {
+        Point { x: self.x * 2, y: self.y * 2 }
+    }
+}
+
 /// Add "The Great" to the end of the name.
 fn greaten(name: String) -> String {
     name + " The Great"
@@ -42,4 +57,7 @@ fn main() {
     let y = 1_000;
     let z = x + y;
     println!("{} {} {}", x, y, z);
+
+    let p = Point::new(1, 2).double();
+    println!("{} {}", p.x, p.y);
 }
